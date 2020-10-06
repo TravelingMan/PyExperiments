f553d6ed85876a4dd7f981fc9af165693c08d56c
S. Hall <shall.usnavy@gmail.com>
2020-10-06 13:23:51 -0400

    Add dragon_adventure.py script

---
+++ games/dragon_adventure.py
@@ -0,0 +1,43 @@
+import random, time
+
+
+def display_intro():
+    print('''You are in a land of Dragons and valuable loot. Before you,
+tucked into the cliffs of a long forgotten mountain, two caves that
+clearly belong to the great wyrms await your advance. One of them
+holds great treasure and a friendlier dragon. The other, death.\n''')
+
+
+def choose_cave():
+    cave = ''
+    while cave != '1' and cave != '2':
+        print('Which terrifying cave do you choose? (1 or 2)')
+        cave = input()
+
+    return cave
+
+
+def check_cave(choosen_cave):
+    print('You steady yourself and approach the cave.')
+    time.sleep(2)
+    print('Darkness surrounds you. A wave of heat slams into your body.')
+    time.sleep(2)
+    print('''The eyes of a terrifying dragon suddenly open, lighting the cave with an ominous, ancient light. The 
+enormous head approaches you and ...''')
+    time.sleep(3)
+
+    friendly_cave = random.randint(1, 2)
+
+    if choosen_cave == str(friendly_cave):
+        print('Offers you a portion of his treasure!')
+    else:
+        print('Snaps his jaws around you, ending your life.')
+
+
+play_again = 'yes'
+while play_again == 'yes' or play_again == 'y':
+    display_intro()
+    cave_number = choose_cave()
+    check_cave(cave_number)
+
+    play_again = input('\nWant to try your luck again? ')
