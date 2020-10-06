7c9236ff6e872d0dc6db9df32710dea5b76d1538
S. Hall <shall.usnavy@gmail.com>
2020-10-06 13:01:04 -0400

    Add collatz script

---
+++ collatz.py
@@ -0,0 +1,24 @@
+def collatz(number):
+    if number % 2 == 0:
+        x = number / 2
+        print(x)
+        return x
+    else:
+        x = 3 * number + 1
+        print(x)
+        return x
+
+
+def get_number():
+    try:
+        user_number = int(input('Enter a number: '))
+        x = collatz(user_number)
+
+        while x != 1:
+            x = collatz(x)
+    except ValueError:
+        print('Please enter a standard integer (whole number)')
+        get_number()
+
+
+get_number()
