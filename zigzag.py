6e803aeaad3ec2950a432bcac3373408ce3938f7
S. Hall <shall.usnavy@gmail.com>
2020-10-06 12:41:57 -0400

    Add zigzag script

---
+++ zigzag.py
@@ -0,0 +1,25 @@
+import time, sys
+
+indent = 0
+indent_increasing = True
+
+try:
+    while True:
+        print(' ' * indent, end='')
+        print('********')
+        time.sleep(0.1)
+
+        if indent_increasing:
+            indent += 1
+            if indent == 20:
+                # Change direction
+                indent_increasing = False
+
+        else:
+            indent -= 1
+            if indent == 0:
+                # Change direction
+                indent_increasing = True
+
+except KeyboardInterrupt:
+    sys.exit()
