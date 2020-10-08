4f33f3565d05038d388c99b935df212de5890570
S. Hall <shall.usnavy@gmail.com>
2020-10-07 22:16:33 -0400

    Add card calculations to blackjack.py

--- games/blackjack.py
+++ games/blackjack.py
@@ -44,6 +44,22 @@ class Player():
 
     def calc_hand(self, dealer_start=True):
         total = 0
+        aces = 0
+        card_values = {1: 1, 2: 2, 3: 3, 4: 4, 5: 5, 6: 6, 7: 7, 8: 8, 9: 9, 10: 10, "J": 10, "Q": 10, "K": 10, "A": 11}
+        if self.name == 'Dealer' and dealer_start:
+            card = self.hand[1]
+            return card_values[card[0]]
+        for card in self.hand:
+            if card[0] == 'A':
+                aces += 1
+            else:
+                total += card_values[card[0]]
+        for i in range(aces):
+            if total + 11 > 21:
+                total += 1
+            else:
+                total += 11
+        return total
 
 
 game = Blackjack()
